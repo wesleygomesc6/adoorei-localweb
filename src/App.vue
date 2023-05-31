37a78818ae06bbaaaea4ca6edf632c07396e1813
wesle.gcarvalho <wesley.gcarvalho@goias.gov.br>
2023-05-31 16:00:15 -0300

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+} */
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'LOCALWEB'
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1> {{ title }} </h1>
+    <div class="formulario">
+      <form>
+        <div class="descricao-form">
+          <h2> Entre na sua Conta </h2>
+          <span>Para acessar sua conta informe seu e-mail e senha</span>
+        </div>
+        <div class="inputs">
+          <label for="lemail">E-mail</label>
+          <input type="email" id="email" name="lemail" placeholder="Seu E-mail" />
+          <label for="lsenha">Senha</label>
+          <input type="password" id="senha" name="lsenha" placeholder="Sua senha" />
+        </div>
+        <div class="recuperar-senha">
+          <span>Esqueci minha senha</span>
+        </div>
+        <input class="submit" type="submit" id="login" title="Fazer Login" value="FAZER LOGIN">
+      </form>
+      <div class="cadastro">
+        <span>Ainda não tem conta? <a href="" class="cadastre-se">Cadastre-se</a></span>
+      </div>
+    </div>
+  </main>
+</template>
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.formulario {
+  border-radius: 5px;
+  background-color: white;
+  padding: 20px;
+  box-shadow: 3px 2px 3px 2px rgba(50, 50, 50, 0.22);
+}
+
+.descricao-form {
+  margin: 20px 0px;
+}
+
+.inputs {
+  margin: 20px 0px;
+}
+
+input {
+  width: 100%;
+  display: block;
+  padding: 5px;
+  height: 50px;
+  margin: 10px 0px;
+  border-radius: 5px;
+}
+
+.recuperar-senha {
+  display: flex;
+  justify-content: end;
+}
+
+input.submit {
+  background-color: var(--cor-primaria);
+  color: white;
+  border: none;
+}
+
+.cadastro {
+  display: flex;
+  justify-content: center;
+  margin: 15px 0px;
+}
+
+.cadastre-se {
+  color: var(--cor-primaria);
+
+}
+</style>
