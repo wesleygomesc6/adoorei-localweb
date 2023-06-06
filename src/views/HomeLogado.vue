32220a6c930022a47e14693a8d913ffac54e4887
wesle.gcarvalho <wesley.gcarvalho@goias.gov.br>
2023-06-06 07:59:11 -0300

    central de ajuda

--- src/views/HomeLogado.vue
+++ src/views/HomeLogado.vue
@@ -4,10 +4,11 @@
             <v-col cols="12" sm="10" md="8">
                 <h1>Olá {{ usuario[0].toUpperCase()+usuario.substring(1) }},</h1>
                 <span class="text-body-1 d-block mb-5">Sejá bem-vindo(a) a sua conta de hospedagem.</span>
-                <iframe class="video" src="https://www.youtube.com/embed/zcXtyKdP92c" title="YouTube video player"
+                <iframe class="video mb-5" src="https://www.youtube.com/embed/zcXtyKdP92c" title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowfullscreen></iframe>
+                    <span>Caso tenha alguma dúvida, <a href="">visite nossa central de ajuda.</a></span>
             </v-col></v-container>
     </LayoutDefault>
 </template>
